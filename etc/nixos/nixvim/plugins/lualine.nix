f38de8639cbc3025624b97ea893793aec40b1bdf
o0th <o0th@pm.me>
2024-09-26 15:16:45 +0200

    feat: lualine like tmux-nova

--- etc/nixos/nixvim/plugins/lualine.nix
+++ etc/nixos/nixvim/plugins/lualine.nix
@@ -5,7 +5,53 @@
     settings = {
       options = {
         component_separators = "";
-        section_separators = "";
+        section_separators = {
+          left = "";
+          right = "";
+        };
+      };
+
+      sections = {
+        lualine_a = [
+          {
+            __unkeyed = "mode";
+            separator = {
+              left = "";
+              right = "";
+              color = {
+                bg = "#282a36";
+              };
+            };
+          }
+        ];
+
+        lualine_c = [
+          {
+            __unkeyed = "filetype";
+            colored = true;
+            icon_only = true;
+            padding = {
+              left = 1;
+              right = 0;
+            };
+          }
+          {
+            __unkeyed = "filename";
+            path = 1;
+          }
+        ];
+
+        lualine_x = [{}];
+        lualine_y = [{}];
+        lualine_z = [
+          {
+            __unkeyed = "location";
+            separator = {
+              left = "";
+              right = "";
+            };
+          }
+        ];
       };
     };
   };
